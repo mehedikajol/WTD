262eec122fcddfdf8c64662e58c5113b312560af
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-05-15 23:49:39 +0600

    adding menu support and menus

--- functions.php
+++ functions.php
@@ -1,5 +1,17 @@
 <?php
 
+function wtd_setup(){
+    load_theme_textdomain('wtd');
+
+    register_nav_menus( 
+        array(
+            'main-menu' => __('Main Menu', 'wtd'),
+            'footer-menu' => __('Footer Menu', 'wtd'),
+        ) 
+    ); 
+}add_action( 'after_setup_theme', 'wtd_setup' );
+
+
 function wtd_scripts() {
     // FONT-AWESOME CSS
     wp_enqueue_style( 'font-awesome', get_template_directory_uri().'/assets/css/font-awesome.min.css', array(), '5.15.3', 'all');
@@ -18,5 +30,4 @@ function wtd_scripts() {
     wp_enqueue_script( 'owl-carousel', get_template_directory_uri().'/assets/js/owl.carousel.min.js', array('jquery'), '2.3.4', true );
     // MAIN JS
     wp_enqueue_script( 'main', get_template_directory_uri().'/assets/js/main.js', array('jquery'), '1.0.0', true );
-}
-add_action('wp_enqueue_scripts', 'wtd_scripts');
+}add_action('wp_enqueue_scripts', 'wtd_scripts');
--- header.php
+++ header.php
@@ -18,13 +18,15 @@
                 </a>
             </div>
             <div class="menu">
-                <ul>
-                    <li><a href="index.html">Home</a></li>
-                    <li><a href="about.html">About</a></li>
-                    <li><a href="services.html">Services</a></li>
-                    <li><a href="gallery.html">Gallery</a></li>
-                    <li><a href="contact.html">Contact</a></li>
-                </ul>
+
+                <?php 
+                    wp_nav_menu(
+                        array(
+                            'theme-location' => 'main-menu',
+                            'menu_class' => 'text'
+                        )
+                    );
+                ?>
             </div>
         </div>
         <!-- Header End Here -->
