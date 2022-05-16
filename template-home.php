38ce08e76d6b1ded230c11cb3ca0a5179be54d2c
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-05-16 23:46:41 +0600

    fixing home page design layout

--- template-home.php
+++ template-home.php
@@ -18,8 +18,36 @@ get_header(); ?>
         <div <?php post_class(array('blogs', 'fix')); ?>>
             <div class="blog-left">
                 <h4>Latest Posts</h4>
-                <?php get_template_part( 'template-parts/content', 'blogs' ); ?>  
+                <div class="blog">
+                    <?php
+                        $args = array(
+                            'post_type'    => 'post',
+                            'posts_per_page' => 6,
+                        );
+                        $query1 = new WP_Query( $args );
+
+                        while ( $query1->have_posts() ) {
+                            $query1->the_post();
+                        ?>
+                            <div class="single-blog home-blog">
+                                <h4><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h4>
+                                <?php the_post_thumbnail('post-thumbnail'); ?>
+                                <div class="blog-meta">
+                                    <?php the_author_posts_link(); ?>
+                                    <?php the_date('M d'); ?>
+                                    <?php the_category(); ?>
+                                </div>
+                                <p><?php the_excerpt(); ?></p>
+                                <a href="<?php the_permalink(); ?>">read more</a>
+                            </div> 
+                        <?php 
+                        }
+                        wp_reset_postdata();
+                    ?>
+                </div> 
             </div>
             <?php get_sidebar(); ?>
         </div>
         <?php get_footer(); ?>
+
+
