d5600b50c03a03ffc1127cf364e0d83c7e81ecc1
Mehedi Hasan Kajol <mehedi.kajol.ice@gmail.com>
2022-05-20 18:23:21 +0600

    service page complete

--- template-about.php
+++ template-about.php
@@ -11,7 +11,7 @@ get_header();?>
         <div class="row">
             <div class="col-xl-12">
                 <div class="breadcumb">
-                    <h4><?php the_title(); ?></h4>
+                    <h4><?php the_title(); ?> Us</h4>
                     <ul>
                         <li><a href="<?php echo site_url(); ?>">Home</a></li> / 
                         <li><?php the_title(); ?></li>
--- template-services.php
+++ template-services.php
@@ -11,10 +11,10 @@ get_header();?>
         <div class="row">
             <div class="col-xl-12">
                 <div class="breadcumb">
-                    <h4>Our Services</h4>
+                    <h4>Our <?php the_title(); ?></h4>
                     <ul>
-                        <li><a href=""></a>Home</li> / 
-                        <li>Our Services</li>
+                        <li><a href="<?php echo site_url(); ?>">Home</a></li> / 
+                        <li><?php the_title(); ?></li>
                     </ul>
                 </div>
             </div>
@@ -26,54 +26,21 @@ get_header();?>
 <section class="services-area pt-100 pb-50" id="services">
          <div class="container">
             <div class="row">
-               <div class="col-lg-4 col-md-6">
-                  <!-- Single Service -->
-                  <div class="single-service">
-                     <i class="fa fa-laptop"></i>
-                     <h4>Web Design </h4>
-                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry typesetting industry </p>
+               <?php 
+                  $services_options = get_field('services_options');
+                  foreach($services_options as $services_option){
+               ?>
+                  <div class="col-lg-4 col-md-6">
+                     <!-- Single Service -->
+                     <div class="single-service">
+                        <i class="fa <?php echo $services_option['service_icon']; ?>"></i>
+                        <h4><?php echo $services_option['service_title']; ?></h4>
+                        <p><?php echo $services_option['service_details']; ?></p>
+                     </div>
                   </div>
-               </div>
-               <div class="col-lg-4 col-md-6">
-                  <!-- Single Service -->
-                  <div class="single-service">
-                     <i class="fa fa-gears"></i>
-                     <h4>Web Development</h4>
-                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry typesetting industry </p>
-                  </div>
-               </div>
-               <div class="col-lg-4 col-md-6">
-                  <!-- Single Service -->
-                  <div class="single-service">
-                     <i class="fa fa-mobile"></i>
-                     <h4>Responsive Design</h4>
-                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry typesetting industry </p>
-                  </div>
-               </div>
-               <div class="col-lg-4 col-md-6">
-                  <!-- Single Service -->
-                  <div class="single-service">
-                     <i class="fa fa-magic"></i>
-                     <h4>Graphic Design</h4>
-                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry typesetting industry </p>
-                  </div>
-               </div>
-               <div class="col-lg-4 col-md-6">
-                  <!-- Single Service -->
-                  <div class="single-service">
-                     <i class="fa fa-pencil"></i>
-                     <h4>Creative Design</h4>
-                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry typesetting industry </p>
-                  </div>
-               </div>
-               <div class="col-lg-4 col-md-6">
-                  <!-- Single Service -->
-                  <div class="single-service">
-                     <i class="fa fa-fa fa-lightbulb-o"></i>
-                     <h4>Branding</h4>
-                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry typesetting industry </p>
-                  </div>
-               </div>
+               <?php
+                  }
+               ?>            
             </div>
          </div>
       </section>
